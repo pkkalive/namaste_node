61e50ecf5f2164ab26e4deea25e73276979da2ba
Purushotham Kumar Kalive <tammu@mac.mynetworksettings.com>
2025-09-06 17:27:40 -0400

    added design pattern: module

--- sum.js
+++
@@ -1,9 +0,0 @@
-console.log('Sum file executed');
-
-// const name = 'Kumar';
-
-export function calculateSum(a, b) {
-    return a + b;
-}
-
-export default calculateSum;
--- xyz.js
+++
@@ -1,1 +0,0 @@
-console.log('XYZ file executed');
