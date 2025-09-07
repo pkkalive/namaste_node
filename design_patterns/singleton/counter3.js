936afa1d47fb2c568f85f3316a4b800adfd4b78d
Purushotham Kumar Kalive <kalivepurushothamkumar@gmail.com>
2025-09-06 23:18:15 -0400

    feat: added singleton_pattern most optimised

---
+++ design_patterns/singleton/counter3.js
@@ -0,0 +1,10 @@
+const counter = (count) =>
+    Object.freeze({
+        getCount: () => count,
+        increment: () => ++count,
+        decrement: () => --count,
+    });
+
+const SingletonCounter3 = counter(0);
+
+export default SingletonCounter3;
